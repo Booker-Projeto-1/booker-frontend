4f14dc68eb234c28896713533232e665bbca7640
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2023-04-26 18:53:20 -0300

    feat: add start configs

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Inter } from "next/font/google";
+import Link from "next/link";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <h1>Home</h1>
-  )
+    <>
+      <h1>Home</h1>
+      <Link href="/login">LOGIN</Link>
+    </>
+  );
 }
