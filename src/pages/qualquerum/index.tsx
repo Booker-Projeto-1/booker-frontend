3b7f533135d42832cebde6eee1114fb9c6fa98fb
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2023-04-26 15:47:05 -0300

    Configuração inicial

---
+++ src/pages/qualquerum/index.tsx
@@ -0,0 +1,10 @@
+type ComponenteProps = {
+    nome: string,
+    idade: number
+}
+
+const Componente = ({nome, idade}: ComponenteProps) => {
+    return <h1>TEXTOOOOOOO</h1>
+}
+
+export default Componente
