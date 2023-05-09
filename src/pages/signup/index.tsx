642cfe5a8963cceba0de2c29680c70b996bf11e9
arthuralv <arthur.alves@ccc.ufcg.edu.br>
2023-05-08 21:30:13 -0300

    set required attribute to required fields

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -1,7 +1,7 @@
 import { AuthContext } from "@/context/AuthContext";
 import { GetServerSideProps, NextPage } from "next";
 import { useContext, useState } from "react";
-import { Button, Container, Form, Input, InputContainer, Logo, LogoContainer, ULink } from "./styles";
+import { Button, Container, Form, Input, InputContainer, FormHelperText, Logo, LogoContainer, FormErrorMessage, FormLabel } from "./styles";
 import { getAPIClient } from "@/services/axios";
 import { parseCookies } from "nookies";
 import Link from "next/link";
@@ -13,6 +13,40 @@ const SignUp: NextPage = () => {
     await signUp(data);
   };
 
+  const [name, setName] = useState('')
+  const [lastname, setLastname] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [phone, setPhone] = useState('')
+
+  const handleNameChange = (e: any) => {
+    setName(e.target.value)
+  }
+
+  const handleLastnameChange = (e: any) => {
+    setLastname(e.target.value)
+  }
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value)
+  }
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value)
+  }
+
+  const handlePhoneChange = (e: any) => {
+    setPhone(e.target.value)
+  }
+
+  const isErrorName = name === '';
+  const isErrorLastname = lastname === '';
+  const isErrorEmail = email === '';
+  const isErrorPassword = password === '';
+  const isErrorPhone = phone === '';
+  const isError = isErrorName || isErrorLastname || isErrorEmail || isErrorPassword || isErrorPhone;
+
+
   return (
     <Container>
       <LogoContainer>
@@ -22,20 +56,57 @@ const SignUp: NextPage = () => {
         </strong>
       </LogoContainer>
       <Form onSubmit={handleSubmit}>
-        <strong>
-          <h1>Fazer Cadastro</h1>
-        </strong>
-        <InputContainer>
-            <Input name="name" type="text" placeholder="Nome" />
-            <Input name="lastname" type="text" placeholder="Sobrenome" />
-            <Input name="email" type="email" placeholder="E-mail" /> 
-            <Input name="password" type="password" placeholder="Senha" />
-            <Input name="password_confirmation" type="password" placeholder="Confirme a senha" />
-            <Input name="phone" type="text" placeholder="Telefone" /> 
-            <Button type="submit">Cadastrar</Button>
+        <FormLabel>Fazer Cadastro</FormLabel>
+        <InputContainer isInvalid={isErrorName}>
+          <Input placeHolder="Nome" type="text" value={name} onChange={handleNameChange} />
+          {!isErrorName ? (
+            null
+          ) : (
+            <FormErrorMessage>Nome é obrigatório.</FormErrorMessage>
+          )}
         </InputContainer>
+        <InputContainer isRequired isInvalid={isErrorLastname}>
+          <Input placeHolder="Sobrenome" type="text" value={lastname} onChange={handleLastnameChange} />
+          {!isErrorLastname ? (
+            null
+          ) : (
+            <FormErrorMessage>Sobrenome é obrigatório.</FormErrorMessage>
+          )}
+        </InputContainer>
+        <InputContainer isRequired isInvalid={isErrorEmail}>
+          <Input placeHolder="Email" type="email" value={email} onChange={handleEmailChange} />
+          {!isErrorEmail ? (
+            null
+          ) : (
+            <FormErrorMessage>Email é obrigatório.</FormErrorMessage>
+          )}
+        </InputContainer>
+        <InputContainer isRequired isInvalid={isErrorPassword}>
+          <Input placeHolder="Senha" type="password" value={password} onChange={handlePasswordChange} />
+          {!isErrorPassword ? (
+            null
+          ) : (
+            <FormErrorMessage>Senha é obrigatório.</FormErrorMessage>
+          )}
+        </InputContainer>
+        <InputContainer isRequired isInvalid={isErrorPhone}>
+          <Input placeHolder="Telefone" type="text" value={phone} onChange={handlePhoneChange} />
+          {!isErrorPhone ? (
+            null
+          ) : (
+            <FormErrorMessage>Telefone é obrigatório.</FormErrorMessage>
+          )}
+        </InputContainer>
+        <Button type="submit" disabled={isError}>
+          Cadastrar
+        </Button>
+        {!isError ? (
+          null
+        ) : (
+          <FormErrorMessage>Por favor, preencha todos os campos.</FormErrorMessage>
+        )}
         <p>
-          Já tem conta?{" "}
+          Possui uma conta?{" "}
           <Link href={"/login"}>
             <strong>Entre</strong>
           </Link>
--- src/pages/signup/styles.ts
+++ src/pages/signup/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { Input as UInput } from "@chakra-ui/react";
+import { FormLabel as FL, Input as UInput, FormControl as FC, FormErrorMessage as FE, FormHelperText as FH} from "@chakra-ui/react";
 
 //If media 320px or less then apply the css below to the component
 export const Container = styled.div`
@@ -20,6 +20,7 @@ export const Container = styled.div`
 `;
 
 export const LogoContainer = styled.div`
+  width: 8rem;
   display: flex;
   flex-direction: row;
   align-items: center;
@@ -61,16 +62,25 @@ export const Form = styled.form`
   background-color: ${(props) => props.theme.palette.secondary.main};
   border-radius: 30px;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
-  */ h1 {
-    margin-bottom: 20px;
+  
+  h1 {
     color: ${(props) => props.theme.palette.primary.contrastText};
-    font-weight: bold;
     font-size: large;
+    margin-bottom: 25px
   }
+
   p {
     color: ${(props) => props.theme.palette.primary.contrastText};
     font-size: large;
+    margin-top: 25px;
+    
   }
+
+  h1, p {
+    @media (max-width: 500px) {
+      margin: 0
+    }
+  }  
   @media (max-width: 500px) {
     width: 100%;
     max-width: 600px;
@@ -88,15 +98,16 @@ export const Input = styled(UInput)`
   border: 1px solid ${(props) => props.theme.palette.secondary.main};
   background-color: ${(props) => props.theme.palette.common.white};
   color: ${(props) => props.theme.textColor};
+
   &::placeholder {
     color: ${(props) => props.theme.textColor};
   }
 `;
 
+
 export const Button = styled.button`
   width: 100%;
   height: 50px;
-  margin-bottom: 10px;
   border-radius: 30px;
   border: 1px solid ${(props) => props.theme.palette.darker.main};
   background-color: ${(props) => props.theme.palette.darker.main};
@@ -109,16 +120,23 @@ export const Button = styled.button`
   }
 `;
 
-export const InputContainer = styled.div`
+export const FormHelperText = styled(FH)`
+  color: black;
+`;
+
+export const FormErrorMessage = styled(FE)`
+  color: red;
+`;
+
+export const InputContainer = styled(FC)`
   width: 100%;
-  height: 26rem;
+  height: 5rem;
   display: flex;
   flex-direction: column;
   align-items: space-between;
   justify-content: space-between;
 `;
 
-export const ULink = styled.link`
-  margin:0;
-  padding:0;
+
+export const FormLabel = styled(FL)`
 `;
