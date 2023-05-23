e8f09a57d9896f1bef5729f4c087b8bbf2372d27
Mateus Matias <mateus.ribeiro@ccc.ufcg.edu.br>
2023-05-23 04:04:05 -0300

    feat: my ads

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,25 +1,48 @@
-import { Icon } from "@chakra-ui/react";
-import { SidebarButton, SidebarButtonGroup, Wrapper } from "./style";
-import { BsHouse, BsBook, BsPerson, BsBoxArrowRight } from 'react-icons/bs';
-import Logo from "../Logo";
-import { useContext } from "react";
 import { AuthContext } from "@/context/AuthContext";
+import { Icon } from "@chakra-ui/react";
+import Link from "next/link";
 import Router from "next/router";
+import { useContext } from "react";
+import { BsBook, BsBoxArrowRight, BsHouse, BsPerson } from "react-icons/bs";
+import Logo from "../Logo";
+import { SidebarButton, SidebarButtonGroup, Wrapper } from "./style";
 
 const Sidebar = () => {
-    const { signOut } = useContext(AuthContext);
+  const { signOut } = useContext(AuthContext);
 
-    return (
-        <Wrapper>
-            <Logo directionColumn showName={false} />
-            <SidebarButtonGroup>
-                <SidebarButton className="sidebarbutton" leftIcon={<Icon as={BsHouse}/>} onClick={() => Router.push('/ads')}>Anúncios</SidebarButton>
-                <SidebarButton className="sidebarbutton" leftIcon={<Icon as={BsBook}/>}>Meus Anúncios</SidebarButton>
-                <SidebarButton className="sidebarbutton" leftIcon={<Icon as={BsPerson}/>}>Meu Perfil</SidebarButton>
-                <SidebarButton className="sidebarbutton" leftIcon={<Icon as={BsBoxArrowRight}/>} onClick={() => signOut()}>Sair</SidebarButton>
-            </SidebarButtonGroup>
-        </Wrapper>
-    );
-}
+  return (
+    <Wrapper>
+      <Logo directionColumn showName={false} />
+      <SidebarButtonGroup>
+        <SidebarButton
+          className="sidebarbutton"
+          leftIcon={<Icon as={BsHouse} />}
+          onClick={() => Router.push("/ads")}
+        >
+          Anúncios
+        </SidebarButton>
+        <SidebarButton
+          className="sidebarbutton"
+          leftIcon={<Icon as={BsBook} />}
+        >
+          <Link href={"/myads"}>Meus Anúncios</Link>
+        </SidebarButton>
+        <SidebarButton
+          className="sidebarbutton"
+          leftIcon={<Icon as={BsPerson} />}
+        >
+          Meu Perfil
+        </SidebarButton>
+        <SidebarButton
+          className="sidebarbutton"
+          leftIcon={<Icon as={BsBoxArrowRight} />}
+          onClick={() => signOut()}
+        >
+          Sair
+        </SidebarButton>
+      </SidebarButtonGroup>
+    </Wrapper>
+  );
+};
 
 export default Sidebar;
--- src/services/advertisement.ts
+++ src/services/advertisement.ts
@@ -1,24 +1,33 @@
 import { api } from "./api";
 
 type newAdRequestData = {
-  bookId: string,
-  description: string
+  bookId: string;
+  description: string;
 };
 
 export async function newAdRequest(data: newAdRequestData) {
-    try {
-        const response = await api.post("/advertisement", data);
-        return Promise.resolve(response.data);
-    } catch (res: any) {
-        return Promise.reject(res.data);
-    }
+  try {
+    const response = await api.post("/advertisement", data);
+    return Promise.resolve(response.data);
+  } catch (res: any) {
+    return Promise.reject(res.data);
+  }
 }
 
 export async function getAds() {
-    try {
-        const response = await api.get("/advertisement/list");
-        return Promise.resolve(response.data);
-    } catch (res: any) {
-        return Promise.reject(res.data);
-    }
+  try {
+    const response = await api.get("/advertisement/list");
+    return Promise.resolve(response.data);
+  } catch (res: any) {
+    return Promise.reject(res.data);
+  }
+}
+
+export async function getUserAds() {
+  try {
+    const response = await api.get("/user/myAdvertisements");
+    return Promise.resolve(response.data);
+  } catch (res: any) {
+    return Promise.reject(res.data);
+  }
 }
